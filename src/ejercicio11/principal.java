79cd02ad44b611fdef6f1a2ea87bec2f5997093e
ibnotis013 <n.ledo@liceolapaz.net>
2025-10-08 09:10:09 +0200

    ejercicio 11 terminado

---
+++ src/ejercicio11/principal.java
@@ -0,0 +1,13 @@
+package ejercicio11;
+
+import java.util.Scanner;
+
+public class principal {
+    static void main() {
+        for (int i = 0; i <= 100; i+=2){
+            System.out.println(i);
+
+        }
+        System.out.println("fin");
+    }
+}
